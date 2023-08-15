9fd52639085b03987464d3f87ef2ff8fbed359bf
danylokotur <kotur.danylo@gmail.com>
2023-08-16 00:49:54 +0300

    HW1

---
+++ HW1/first.js
@@ -0,0 +1,68 @@
+let a;
+a = `hello`;
+console.log(a);
+
+let b;
+b = `owu`;
+console.log(b);
+
+let c;
+c = `com`;
+console.log(c);
+
+let d;
+d = `ua`;
+console.log(d);
+
+let e;
+e = `1`;
+console.log(e);
+
+let f;
+f = `10`;
+console.log(f);
+
+let g;
+g = `-999`;
+console.log(g);
+
+let h;
+h = `123`;
+console.log(h);
+
+let i;
+i = `3.14`;
+console.log(i);
+
+let j;
+j = `2.7`;
+console.log(j);
+
+let k;
+k = `16`;
+console.log(k);
+
+let l;
+l = `true`;
+console.log(l);
+
+let m;
+m = `false`;
+console.log(m);
+
+
+let firstName =`Danylo`;
+let middleName=`Romanovych`;
+let lastName=`Kotur`;
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+let a1=100;
+let b1=`100`;
+let c1=true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+
