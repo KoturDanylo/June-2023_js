d712020ba3d8db62fbc75bc1a96b89eabdfc0482
danylokotur <kotur.danylo@gmail.com>
2023-08-21 19:26:27 +0300

    HW1

---
+++ HW2/first.js
@@ -0,0 +1,88 @@
+let cars=[
+    "BMW" ,
+    "Mercedes" ,
+    "audi" ,
+    "tesla" ,
+    "skoda" ,
+    "vw" ,
+    "seat" ,
+    "renault" ,
+    "porsche" ,
+    "fiat" ,
+];
+console.log(cars);
+console.log(cars[5]);
+console.log(cars[8]);
+console.log(cars[0]);
+
+// ------------------------------------------------//
+
+
+let book1={
+    title:'The Adventures of Sherlock Holmes',
+    pageCount: 320,
+    genre: 'detectiveStory'
+}
+let book2 ={
+    title:'Holly',
+    pageCount: 363,
+    genre: 'horror'
+}
+
+let book3 ={
+    title:'harry potter',
+    pageCount: 500,
+    genre: 'fantasy'
+}
+
+
+book1.author = {
+    name: 'Arthur Conan Doyle',
+    age: 71
+}
+console.log(book1)
+
+book2.author = {
+    name: 'Stephen King',
+    age: 75
+}
+console.log(book2)
+
+book3.author = {
+    name: 'J. K. Rowling',
+    age: 58
+}
+console.log(book2)
+
+
+// -----------------------------------------------/
+
+
+let people = [
+    {name: 'Max', username:'max1', password: 2344332},
+    {name: 'Lera', username: 'lera1', password: 556868},
+    {name: 'Danylo', username: 'dan1', password: 78957647},
+    {name: 'Sasha', username: 'sasha1', password: 902142},
+    {name: 'oleg', username: 'oleg11', password: 45465457},
+    {name: 'Olya', username: 'olya1', password: 568758},
+    {name: 'Maryana', username: 'mar1', password: 6574574},
+    {name: 'Roman', username: 'roman1', password: 789678},
+    {name: 'Daryna', username: 'dar1', password: 768678},
+    {name: 'Bogdan', username: 'bogdan1', password: 14243},
+
+]
+
+console.log(people[0].password)
+console.log(people[1].password)
+console.log(people[2].password)
+console.log(people[3].password)
+console.log(people[4].password)
+console.log(people[5].password)
+console.log(people[6].password)
+console.log(people[7].password)
+console.log(people[8].password)
+console.log(people[9].password)
+
+
+
+
---
+++ HW2/if.js
@@ -0,0 +1,70 @@
+// let time = prompt( 'Hello')
+//
+// if (time>=0 && time<=14)
+//     console.log('1')
+// if (time>=15 && time<=29)
+//     console.log('2')
+// if (time>=30 && time<=44)
+//     console.log('3')
+// if (time>=45 && time<=59)
+//     console.log('4')
+
+
+// -------------------------------------------/
+// let Day = prompt( 'Day')
+//
+// if (Day>=1 && Day<=11)
+//     console.log('1')
+// if (Day>=12 && Day<=21)
+//     console.log('2')
+// if (Day>=22 && Day<=31)
+//     console.log('3')
+
+
+// -------------------------------------------/
+
+
+// let week =prompt("Which day you choose?")
+// switch (week) {
+//     case '1':
+//         console.log('Monday');
+//         break;
+//     case '2':
+//         console.log('Tuesday');
+//         break;
+//     case '3':
+//         console.log('Wednesday');
+//         break;
+//     case '4':
+//         console.log('Thursday');
+//         break;
+//     case '5':
+//         console.log('Friday');
+//         break;
+//     case '6':
+//         console.log('Saturday');
+//         break;
+//     case '7':
+//         console.log('Sunday');
+//         break;
+//
+//     default:
+//         console.log("fjrnnjfr")
+//
+// }
+
+// -------------------------------------------/
+
+
+let coursesAndDurationArray=  [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+    ];
+
+if (coursesAndDurationArray[3].monthDuration>=5)
+    console.log('super')
+console.log(coursesAndDurationArray[3].monthDuration >=5)
