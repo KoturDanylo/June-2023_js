438d1729387102f589eb7c46d6fc24c52c1d07cc
danylokotur <kotur.danylo@gmail.com>
2023-08-29 00:27:40 +0300

    HW2

--- HW4/first.js
+++ HW4/first.js
@@ -1,30 +1,40 @@
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 
-function users(id, name, age) {
-    document.write(
-
-        `<div>
-        <p>id:${id}</p>
-            <p>name:${name}</p>
-               <p>age:${age}</p>
-               </div>`
-);
-}
-
-users(1, 'Danylo', 23 )
+// function users(id, name, age) {
+//     document.write(
+//
+//         `<div>
+//         <p>id:${id}</p>
+//             <p>name:${name}</p>
+//                <p>age:${age}</p>
+//                </div>`
+// );
+// }
+//
+// users(1, 'Danylo', 23 )
 
 // -------------------------------------------/
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
-// function aaa(a, b, c) {
-//     console.log(a, b, c)
+// const users =[
+//     {id:1, name: 'vasya', age: 31},
+//     {id:1, name: 'petya', age: 30},
+//     {id:1, name: 'kolya', age: 29},
+//     {id:1, name: 'olya', age: 28},
+//     {id:1, name: 'max', age: 30},
+//     {id:1, name: 'anya', age: 31}
+//     ];
+// function userPresentor(arr) {
+//     for (const user of arr) {
+//         document.write(`<di>
+//                 <p>id:${user.id}</p>
+//                 <p>name:${user.name}</p>
+//                 <p>age:${user.age}</p>
+//                 </di>`)
+//     }
 // }
-//
-// aaa(23, 'Danylo', true )
-// aaa(19, 'Maryana', false )
-
-
+// userPresentor(users)
 // -------------------------------------------/
 
 // - створити функцію яка повертає найменьше число з масиву
@@ -113,12 +123,74 @@ users(1, 'Danylo', 23 )
 // }
 // users([])
 
+
+
 // -------------------------------------------/
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
-function user( text) {
-    document.write(`<p>%{text}</p>`)
-    
+// function user( text) {
+//     document.write(`<p>${text}</p>`)
+//
+// }
+// user('Danylo')
+
+// -------------------------------------------/
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+// function uluser( text) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i< 3; i++) {
+//         document.write(`<li>${text}</li>`);
+//
+//     }
+//     document.write(`</ul>`);
+// }
+// uluser('Danylo')
+
+
+// -------------------------------------------/
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// function uluser( text, count) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i< count; i++) {
+//         document.write(`<li>${text}</li>`);
+//
+//     }
+//     document.write(`</ul>`);
+// }
+// uluser('Danylo',4);
+// uluser('Danylo',5);
+
+// -------------------------------------------/
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+// function swap(arr, index1,index2) {
+//     const numIndex1=arr[index1];
+//     const numIndex2=arr[index2];
+//
+//     arr[index1]=numIndex2;
+//     arr[index2]=numIndex1;
+//
+//     return arr;
+// }
+//
+//  console.log(swap([11,22,33,44],0,1));
+
+// -------------------------------------------/
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+function exchange(sum,currencies,resultCurrency) {
+    for (const cur of currencies) {
+        if(cur.currency===resultCurrency){
+           return  sum/cur.value
+        }
+
+    }
 }
-user('Danylo')
+
+const result  =exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD')
+console.log(`result :`, result)
