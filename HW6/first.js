c12f47f5474b81027cc41dc9f4221a3280303b72
danylokotur <kotur.danylo@gmail.com>
2023-09-04 20:15:46 +0300

    HW6

--- HW6/first.js
+++ HW6/first.js
@@ -27,11 +27,16 @@
 // console.log(str2.toLowerCase())
 // console.log(str3.toLowerCase())
 // // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-// let str = ' dirty string   '
+let str = ' dirty string   '
 // let strReplace=str.
 //     replaceAll(` `,``);
 //
 // console.log(strReplace)
+// -----------------------------------------------------
+console.log(str.trim());
+const strReplace = str.split(` `).filter((c) =>c.length).join(` `);
+console.log(strReplace)
+
 //
 // // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
@@ -232,88 +237,88 @@
 // --написати пошук всіх об'єктів, в який в modules є sass
 // --написати пошук всіх об'єктів, в який в modules є docker
 
-let coursesArray = [
-    {
-        title: 'JavaScript Complex',
-        monthDuration: 5,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-    },
-    {
-        title: 'Java Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'angular',
-            'aws',
-            'docker',
-            'git',
-            'java core',
-            'java advanced']
-    },
-    {
-        title: 'Python Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'angular',
-            'aws',
-            'docker',
-            'python core',
-            'python advanced']
-    },
-    {
-        title: 'QA Complex',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-    },
-    {
-        title: 'FullStack',
-        monthDuration: 7,
-        hourDuration: 909,
-        modules: ['html',
-            'css',
-            'js',
-            'mysql',
-            'mongodb',
-            'react',
-            'angular',
-            'aws',
-            'docker',
-            'git',
-            'node.js',
-            'python',
-            'java']
-    },
-    {
-        title: 'Frontend',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
-    }
-];
-
-let coursesSASS= coursesArray.filter(function (course) {
-    return course.modules.includes(`sass`)
-
-})
-console.log(coursesSASS);
-
-let coursesDOCKER= coursesArray.filter(function (course) {
-    return course.modules.includes(`docker`);
-
-})
-
-console.log(coursesDOCKER)
-
-// --написати пошук всіх об'єктів, в який в modules є sass
-// --написати пошук всіх об'єктів, в який в modules є docker
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+// let coursesSASS= coursesArray.filter(function (course) {
+//     return course.modules.includes(`sass`)
+//
+// })
+// console.log(coursesSASS);
+//
+// let coursesDOCKER= coursesArray.filter(function (course) {
+//     return course.modules.includes(`docker`);
+//
+// })
+//
+// console.log(coursesDOCKER)
+//
+// // --написати пошук всіх об'єктів, в який в modules є sass
+// // --написати пошук всіх об'єктів, в який в modules є docker
